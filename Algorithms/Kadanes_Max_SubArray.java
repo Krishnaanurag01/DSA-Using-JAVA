5e976d3f1e48c966fed4c737799f0e3b92f6b0d1
Krishnaanurag01 <krishna.anurag02@gmail.com>
2021-07-13 22:25:37 +0530

    DSA Implementation and Problems

---
+++ Algorithms/Kadanes_Max_SubArray.java
@@ -0,0 +1,24 @@
+package Algorithms;
+
+public class Kadanes_Max_SubArray {
+    public static void main(String[] args) {
+        int[] arr = {4,3,-2,6,-12,7,-1,6};
+        System.out.print(Max_SubArray(arr));
+        
+    }
+    public static int Max_SubArray(int[] arr) {
+        int currentMax=arr[0];
+        int maxSoFar=arr[0];
+        for(int i = 1; i<arr.length ; i++){
+            currentMax += arr[i];
+            if(currentMax < arr[i]){
+                currentMax = arr[i];
+            }
+            if(maxSoFar < currentMax){
+                maxSoFar = currentMax;
+            }}
+            return maxSoFar;       
+    }
+
+    
+}
